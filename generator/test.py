fed2c9fdbf006fb0ef28b3e73d062dfba659ddd4
Tom Hills <cugzhangtao@163.com>
2017-04-15 09:08:28 +0800

    生成器与lambda

--- generator/test.py
+++ generator/test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 通过列表生成式，我们可以直接创建一个列表。但是，受到内存限制，列表容量肯定是有限的。
 在Python中，这种一边循环一边计算的机制，称为生成器：generator。
 
 第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator
 定义generator的另一种方法。如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator：
-'''
+"""
 g = (x*x for x in range(1, 11))
 print type(g)
 for n in g:
